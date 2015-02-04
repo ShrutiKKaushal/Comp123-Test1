9d0406b3bce4d13d705cf5f70f24301657903588
akashruti <shruti_01@live.com>
2015-02-04 09:02:52 -0500

    Added car class and its functionality

---
+++ Test1-CarClass/Car.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1_CarClass
+{
+    class Car
+    {
+        //Private properties ++++++++++++++++++++
+        private string Make;
+        private string Model;
+        private string Year;
+        private string Colour;
+
+        // Constructor Method ++++++++++++++++++++
+        public Car(string Make, string Model, string Year, string Colour)
+        {
+            this.Make = Make;
+            this.Model = Model;
+            this.Year = Year;
+            this.Colour = Colour;
+
+
+            //Method Call
+            show();
+        }
+
+
+     
+        //Public Methods  +++++++++++++++++++++++++++++++++
+        public void drive()
+         {
+            Console.WriteLine("The car is Driving");
+         }
+
+        public void show()
+        {
+
+            Console.WriteLine(" ********************* ");
+            Console.WriteLine(" Car Make : {0}", this.Make);
+            Console.WriteLine(" Car Model : {0}", this.Model);
+            Console.WriteLine(" Car Year : {0}", this.Year);
+            Console.WriteLine("Car Colour : {0}", this.Colour);
+            Console.WriteLine(" ********************* ");
+        }
+    }
+}
